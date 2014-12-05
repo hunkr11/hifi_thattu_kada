b52c9d275844d4896ac158cf04c203b47bf24264
shiva <hunkr11@gmail.com>
2014-12-05 10:14:04 +0530

    hifi_thattu_kada

---
+++ hifi_thattu_kada/src/main/java/com/hifi/thattukada/Application.java
@@ -0,0 +1,17 @@
+package com.hifi.thattukada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
+public class Application {
+
+    public static void main(String[] args) {
+    	System.out.println("- -MAIN START- -\n \n");
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ hifi_thattu_kada/src/main/java/com/hifi/thattukada/controller/HelloWorldController.java
@@ -0,0 +1,22 @@
+package com.hifi.thattukada.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HelloWorldController {
+
+ @RequestMapping(value = "/hello", method = RequestMethod.GET)
+ public ModelAndView hello() {
+	 System.out.println(" \n\n -- CONTROLLER STARTED -- \n \n");
+  return new ModelAndView("hello").addObject("name", "Siva");
+ }
+
+ @RequestMapping(value="/",method = RequestMethod.GET)
+ public ModelAndView indexPage(){
+	 return new ModelAndView("index");
+ }
+ 
+}
